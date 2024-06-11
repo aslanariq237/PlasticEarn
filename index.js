ca69a129af5bf300a4636330e01dc8e2a90eed39
aslancrud <60170799+aslanariq237@users.noreply.github.com>
2024-06-11 16:19:34 +0700

    baru-2

---
+++ index.js
@@ -0,0 +1,21 @@
+function setor() {
+  window.location.href = "./setor.html"
+}
+
+function reedem(){
+  window.location.href = './reedem.html'
+}
+
+function store() {
+  window.location.href = './store.html'
+}
+
+
+function myFunction() {
+    var x = document.getElementById("myNav");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+}
